0a738bc096e5844d339f9050ab37cca33d1bf1a1
Pong Cheecharern <k.cheecharern@gmail.com>
2019-03-26 22:43:20 +0700

    answered part 3

--- set_01/part_3/part_3.go
+++ set_01/part_3/part_3.go
@@ -4,23 +4,88 @@
 package part_3
 
 import (
-	"fmt"
 	"encoding/hex"
 	"strings"
 )
 
+type KeyToScore struct {
+	key       string
+	plainText string
+	score     float64
+}
 
-func SingleByteXor() {
-	
-	
-	a := strings.Split(strings.ToUpper("abcdefghijklmnopqrstuvwxyz"), "")
+type KeysToScore []KeyToScore
+
+var s string = "!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+
+func BruteForceDecrypt(encryptedData string) KeysToScore {
+	a := strings.Split(s, "")
+	ks := []KeyToScore{}
 	for _, v := range a {
-		data := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-		res, _ := hex.DecodeString(data)
-
+		res, _ := hex.DecodeString(encryptedData)
 		decrypted := EncryptDecrypt(string(res), v)
-		fmt.Println("Decrypted:", string(decrypted))
+		k := KeyToScore{
+			key:       v,
+			plainText: string(decrypted),
+			score:     0,
+		}
+		ks = append(ks, k)
 	}
+	return ks
+}
+
+func (ks KeysToScore) ScoreTexts() KeysToScore {
+	var nks KeysToScore
+
+	for i := 0; i < len(ks); i++ {
+		val := ks[i]
+		txt := val.plainText
+		for _, ch := range txt {
+			val.score = ScoreText(val.score, ch)
+		}
+		nks = append(nks, val)
+	}
+	return nks
+}
+
+func ScoreText(score float64, char rune) float64 {
+	var s float64
+	cc := map[string]float64{
+		"e": 13,
+		"t": 12,
+		"a": 11,
+		"o": 10,
+		"i": 9,
+		"n": 8,
+		"s": 7,
+		"h": 6,
+		"r": 5,
+		"d": 4,
+		"l": 3,
+		"u": 2,
+	}
+	if cc[string(char)] == 0 {
+		s = score - 5
+	} else {
+		s = score + cc[string(char)]
+	}
+	return s
+}
+
+func (ks KeysToScore) GetHighScore() KeyToScore {
+	var as float64
+	var ap string
+	var ak string
+	for i, _ := range ks {
+		k := ks[i]
+		if k.score > as {
+			as = k.score
+			ak = k.key
+			ap = k.plainText
+		}
+	}
+
+	return KeyToScore{key: ak, plainText: ap, score: as}
 }
 
 func EncryptDecrypt(input, key string) (output string) {
@@ -30,4 +95,3 @@ func EncryptDecrypt(input, key string) (output string) {
 
 	return output
 }
-
--- set_01/part_3/part_3_test.go
+++ set_01/part_3/part_3_test.go
@@ -1,9 +1,20 @@
 package part_3
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestSingleByteXor(t *testing.T) {
-	SingleByteXor()
+	encryptedDataString := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	ak := "X"
+	ap := "Cooking MC's like a pound of bacon"
+	s := BruteForceDecrypt(encryptedDataString)
+	n := s.ScoreTexts()
+	a := n.GetHighScore()
+	if a.key != ak || a.plainText != ap {
+		t.Error("Not correct answer")
+	} else {
+		fmt.Printf("the key is %v \n decrypted plaintext is %s \n", a.key, a.plainText)
+	}
 }
